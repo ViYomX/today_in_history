b026dcd21e66d13cc52eb3c3f0fcca2fa48ec7a4
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-10-18 12:08:21 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -9,33 +9,26 @@ def scrape_indianage():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-    
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
-
     title = soup.find('title').text.strip()
-
     events = {}
     for box in soup.find_all('div', class_='timeline_box'):
         date = box.find('div', class_='date').text.strip()
         event = box.find('p').text.strip()
         events[date] = event
-
     data = {
         "title": title,
         "events": events
     }
-
     return data
-
 
 if __name__ == "__main__":
     data = scrape_indianage()
     india_timezone = pytz.timezone('Asia/Kolkata')
     india_time = datetime.now(india_timezone)
     today_date_month = f"{india_time.day}_{india_time.month}"
-
     file_path = f"today_in_history/{today_date_month}.json"
     with open(file_path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
